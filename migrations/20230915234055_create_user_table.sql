f227544c5a72b016ddcaf01a4cb96e8f49594f05
kerim cetinbas <a.cetinbas@icloud.com>
2023-10-03 01:15:18 +0300

    basic pr create

--- migrations/20230915234055_create_user_table.sql
+++ migrations/20230915234055_create_user_table.sql
@@ -2,32 +2,42 @@
 -- +goose StatementBegin
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TYPE AccountType AS ENUM ('LEGAL', 'REAL');
-create TYPE ResourceType AS ENUM ('IMAGE', 'DOCUMENT');
-
-CREATE TABLE IF NOT EXISTS Accounts (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    password BYTEA NOT NULL,
-    user_name VARCHAR(64),
-    account_type AccountType NOT NULL DEFAULT 'LEGAL',
-    fist_name VARCHAR(32) NOT NULL,
-    last_name VARCHAR(32) NOT NULL
+CREATE TYPE TokenType AS ENUM (
+    'VALIDATION',
+    'AUTHENTICATION'
 );
-
+create TYPE ResourceType AS ENUM ('IMAGE', 'DOCUMENT');
+CREATE Type AccountType AS ENUM ('LEGAL', 'REAL');
+CREATE Type ResourceEncodingType AS ENUM (
+    'SRC',    -- file path
+    'BASE64', -- Base64
+    'BYTEA'   -- byte array
+);
+CREATE TABLE IF NOT EXISTS Accounts (
+    id UUID PRIMARY KEY NOT NULL,
+    password BYTEA NOT NULL,
+    account_type AccountType DEFAULT 'REAL',
+    email VARCHAR(64)
+);
 
 CREATE TABLE IF NOT EXISTS Resources (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     resource_type ResourceType DEFAULT 'IMAGE',
     source TEXT NOT NULL,
     alt TEXT,
+    resource_encoding ResourceEncodingType DEFAULT 'SRC',
+    mime VARCHAR(16),
     description TEXT
 );
 CREATE TABLE IF NOT EXISTS Profiles (
     id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    fk_profile_photo BIGSERIAL NOT NULL,
+    profile_photo_id BIGSERIAL NOT NULL,
+    account_id UUID,
     PRIMARY KEY(id),
-    CONSTRAINT fk_profile_photo 
-        FOREIGN KEY(fk_profile_photo) REFERENCES Resources(id) ON DELETE CASCADE
+    CONSTRAINT fk_profile_photo_id 
+        FOREIGN KEY(profile_photo_id) REFERENCES Resources(id),
+    CONSTRAINT fk_account_id
+        FOREIGN KEY(account_id) REFERENCES Accounts(id)
 );
 
 CREATE TABLE IF NOT EXISTS JK_Friends (
@@ -41,28 +51,34 @@ CREATE TABLE IF NOT EXISTS JK_Friends (
     CONSTRAINT fk_friend_profile
         FOREIGN KEY(fk_friend_profile)
         REFERENCES Profiles(id)
-        ON DELETE NO ACTION
 );
 
+CREATE TABLE IF NOT EXISTS Tokens (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    account_id UUID,
+    value TEXT,
+    token_type TokenType DEFAULT 'AUTHENTICATION',
+    description TEXT,
+
+    CONSTRAINT fk_account_id
+        FOREIGN KEY (account_id) REFERENCES Accounts (id)
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 
+DROP TABLE IF EXISTS Tokens;
 DROP TABLE IF EXISTS JK_Friends;
+DROP TABLE IF EXISTS Profiles;
 
 DROP TABLE IF EXISTS Accounts;
 
-DROP TABLE  IF EXISTS Profiles;
-
-
 
 DROP TABLE IF EXISTS Resources;
--- +goose StatementEnd
--- +goose StatementBegin
-DROP EXTENSION IF EXISTS "uuid-ossp";
-DROP TYPE IF EXISTS ResourceType;
 DROP TYPE IF EXISTS AccountType;
-DROP TYPE IF EXISTS accountstatus;
-DROP TYPE IF EXISTS accounttier;
+DROP TYPE IF EXISTS ResourceEncodingType;
+DROP TYPE IF EXISTS ResourceType;
+DROP TYPE IF EXISTS TokenType;
+DROP EXTENSION IF EXISTS "uuid-ossp";
 -- +goose StatementEnd
